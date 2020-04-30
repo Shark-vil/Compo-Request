faeb46fa3c960f67dc1965b83eb38bc15069b5f3
Shark_vil <sharkvil228@gmail.com>
2020-05-01 00:39:59 +0300

    Полная настройка шаблона окна проектов

--- Compo-Request/Windows/Projects/ProjectsWindow.xaml.cs
+++ Compo-Request/Windows/Projects/ProjectsWindow.xaml.cs
@@ -38,6 +38,20 @@ namespace Compo_Request.Windows.Projects
         {
             // Регистрация события при закрытии главного окна регистрации
             this.Closing += ProjectsWindow_Closing;
+            this.Button_OpenMenu.Click += Button_OpenMenu_Click;
+            this.Button_CloseMenu.Click += Button_CloseMenu_Click;
+        }
+
+        private void Button_CloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Button_OpenMenu.Visibility = Visibility.Visible;
+            Button_CloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_OpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Button_OpenMenu.Visibility = Visibility.Collapsed;
+            Button_CloseMenu.Visibility = Visibility.Visible;
         }
 
         private void ProjectsWindow_CloseEvvent()
