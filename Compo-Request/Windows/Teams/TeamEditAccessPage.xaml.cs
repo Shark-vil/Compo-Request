dd3c528ab1a3d783aa95ac3a008ecb8b54448328
Shark_vil <sharkvil228@gmail.com>
2020-05-26 19:03:51 +0300

    Добавление страницы привилегий для команд

---
+++ Compo-Request/Windows/Teams/TeamEditAccessPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compo_Request.Windows.Teams
+{
+    /// <summary>
+    /// Логика взаимодействия для TeamEditAccessPage.xaml
+    /// </summary>
+    public partial class TeamEditAccessPage : Page
+    {
+        public TeamEditAccessPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
